573dabde9792929ad8976988ebe914ff994c4466
apeksha7777 <apekshavreddy@gmail.com>
2020-07-21 21:33:20 +0530

    html

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,44 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
+import Main from '../components/Main.js';
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadweb3();
+   // await this.loadblockchaindata();
+    
+  }
+  async loadweb3()
+  {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+ }
+
+ async loadblockchaindata(){
+  const web3=window.web3;
+  const accounts=await web3.eth.getAccounts();
+ }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
+     
+
+      <Main></Main>
+    
       </div>
+    
     );
   }
 }
---
+++ src/components/Main.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render(){
+        return(
+            <div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+<link rel="stylesheet" href="App.css"></link>
+<body  class="page2">
+    <div class="header">
+    <h1>power to power</h1>
+    </div>
+    <p>Units of energy</p>
+    <p>Account balance</p>
+<div id="main-wrapper" >
+
+<div class="inner_container">
+             
+            
+            <label htmlFor="surplus">Enter surplus in units</label>
+            <input type="number" id="surplus" name="name" rows="1" cols="10"></input>
+            <button class="back_btn" id="surplusButton">GO</button>
+            <label htmlFor="request">Buy units</label>
+            <input type="number" id="Buy" name="buyunits" rows="1" cols="10"></input>
+            <button class="back_btn" id="BuyEnergy">GO</button>
+           
+</div>
+</div>
+
+</body>
+           
+           
+            </div>
+            
+
+            );
+
+    }
+   
+}
+export default Main;
