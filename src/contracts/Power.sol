64e035a82ba8bc7039754c25f84a9a3e09d36bf5
AnanyaSharmaPy <ananyas9919@gmail.com>
2020-07-22 23:27:54 +0530

    Updating contracts

--- src/contracts/Power.sol
+++ src/contracts/Power.sol
@@ -21,7 +21,7 @@ contract Power {
 	mapping (address => user) Users;
 	mapping (address => grid) Grid;
 	
-	address payable gridAddress = address(GRID_ADDRESS);
+	address payable gridAddress = address(PASTE_GRID_ADDRESS_HERE);
 	uint256 unitPrice = 100;
 	
 	event surplusSent (address energySender, address _gridAddress, uint256 _surplus);
@@ -29,21 +29,24 @@ contract Power {
 	
 	function userJoined () public {
 	    require(Users[msg.sender].isJoined == false, "Already joined");
-	    Users[msg.sender].isJoined = false;
+	    Users[msg.sender].isJoined = true;
 	    Users[msg.sender].energy = 100;
 	}
 	
-	function sendSurplus(uint256 _surplusEnergy) public payable {
-	    require(_surplusEnergy != 0, "Enter right value");
+	function sendSurplus(uint256 _surplusEnergy) payable public returns(address, uint256) {
+	    require(_surplusEnergy > 0, "Enter right value");
+	    require(Users[msg.sender].isJoined == true, "You haven't joined the network");
 	    require(Users[msg.sender].energy >= _surplusEnergy, "Not enough energy to send");
 	    require(Grid[gridAddress].energyStored + _surplusEnergy <= Grid[gridAddress].capacity, "Exceeds grid capacity");
 	    Users[msg.sender].energy -= _surplusEnergy;
 	    Grid[gridAddress].energyStored += _surplusEnergy;
         uint256 amount = unitPrice * _surplusEnergy;
-        msg.sender.transfer(amount);
-        // msg.sender.call.value(amount);
-        // msg.sender.call{value: 1}()
 	    emit surplusSent (msg.sender, gridAddress, _surplusEnergy);
+	    return (msg.sender, amount);
+	}
+	
+	function sendFund(address payable _to, uint256 _amount) payable public {
+	    _to.transfer(_amount);
 	}
 	
 	function retieveBalance () public view returns (uint256) {
@@ -55,7 +58,8 @@ contract Power {
 	}
 	
 	function requestEnergy (uint256 _energyRequired) payable public {
-	    require(_energyRequired != 0, "Enter right value");
+	    require(_energyRequired > 0, "Enter right value");
+	    require(Users[msg.sender].isJoined == true, "You haven't joined the network");
 	    require(msg.value >= unitPrice * _energyRequired, "Please send the right amount");
 	    require(Grid[gridAddress].energyStored >= _energyRequired, "Energy not available in grid");
 	    Grid[gridAddress].energyStored -= _energyRequired;
